8e21e501bcf3639876555f6237266402ecdb486c
Igor Dzilit igor.dzilit@yahoo.com <igor.dzilit@yahoo.com>
2020-11-17 15:57:39 +0100

    pundit for the instrument

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,5 +1,46 @@
 class InstrumentsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @instruments = Instrument.all
+    @instruments = policy_scope(Instrument).order(created_at: :desc)
+  end
+
+  def show
+    @instrument = Instrument.find(params[:id])
+    authorize @instrument
+  end
+
+  def new
+    @instrument = Instrument.new
+    authorize @instrument
+  end
+
+  def create
+    @instrument = Instrument.new(instrument_params)
+    # @instrument.name = params[:name]
+    @instrument.user = current_user
+    authorize @instrument
+    if @instrument.save!
+      redirect_to instrument_path(@instrument)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @instrument = Instrument.find(params[:id])
+    authorize @instrument
+  end
+
+  def destroy
+    @instrument = Instrument.find(params[:id])
+    @instrument.destroy
+    redirect_to instruments_path
+    authorize @instrument
+  end
+
+  private
+
+  def instrument_params
+    params.require(:instrument).permit(:name, :description, :price_per_day)
   end
 end
