1881f14ff15f1f38750b7a59b2e76713c118cfee
clara wolff <cwo@systemli.org>
2020-11-18 13:12:58 +0100

    created a dashboard page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :instruments
+  # the rentals below refer to rentals where the user is the renter
+  has_many :rentals
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
